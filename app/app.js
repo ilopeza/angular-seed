6da54e1b4f0e6f364e317ecbade938f55328aacc
ilopeza <ilopezarzuaga@gmail.com>
2018-08-08 08:36:48 -0300

    First commit

--- app/app.js
+++ app/app.js
@@ -5,10 +5,17 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'ngMaterial'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+.controller('AppCtrl', function() {
+         this.userState = '';
+         this.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+             'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+             'WY').split(' ').map(function (state) { return { abbrev: state }; });
+       });;
